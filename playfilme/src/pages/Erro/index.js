68284b55592fb0f319a6a2563f4db355256355c2
Jennifer Silva <jenni75dr@gmail.com>
2021-02-22 12:37:10 -0300

    Projeto playfilme/filmaria

---
+++ playfilme/src/pages/Erro/index.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Erro extends Component {
+    render(){
+        return(
+            <div>
+                <h1>Pagina não encontrada!</h1>
+            </div>
+        )
+    }
+}
+export default Erro;
